b2a1dd6dcf1f801814cb5a6049d06a5112efed50
viva68 <acbd68@naver.com>
2021-08-10 13:52:22 +0900

    add code

--- ANN.py
+++ ANN.py
@@ -1,5 +1,6 @@
 import numpy
 import scipy.special
+import matplotlib.pyplot
 
 class neuralNetwork:
 
@@ -13,9 +14,9 @@ class neuralNetwork:
         self.activation_function=lambda x: scipy.special.expit(x)
         pass
 
-    def train(self,input_list,target_list):
+    def train(self,input_list,targets_list):
         inputs=numpy.array(input_list,ndmin=2).T
-        targets = numpy.array(target_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
         hidden_inputs = numpy.dot(self.wih, inputs)
 
         hidden_ouputs = self.activation_function(hidden_inputs)
@@ -26,7 +27,7 @@ class neuralNetwork:
 
         output_errors=targets-final_outputs
         hidden_errors=numpy.dot(self.who.T,output_errors)
-        self.who += self.lr * numpy.dot((output_errors*final_outputs*(1.0-final_outputs)),numpy.transpose(inputs))
+        self.who += self.lr * numpy.dot((output_errors*final_outputs*(1.0-final_outputs)),numpy.transpose(hidden_ouputs))
         self.wih += self.lr * numpy.dot((hidden_errors * hidden_ouputs * (1.0 - hidden_ouputs)),numpy.transpose(inputs))
         pass
     def query(self,input_list):
@@ -45,13 +46,43 @@ class neuralNetwork:
         pass
 
 
-input_nodes=3
-hidden_nodes=3
-output_nodes=3
-
+input_nodes=784
+hidden_nodes=100
+output_nodes=10
 learning_rate=0.3
 
-n=neuralNetwork(input_nodes,hidden_nodes,output_nodes,learning_rate)
 
-print(n.query([1.0,0.5,-1.5]))
+
+n=neuralNetwork(input_nodes,hidden_nodes,output_nodes,learning_rate)
+training_data_file=open("mnist_dataset/mnist_train_100.csv",'r')
+training_data_list=training_data_file.readlines()
+training_data_file.close()
+
+
+
+for record in training_data_list:
+    all_values=record.split(',')
+    inputs=(numpy.asfarray(all_values[1:])/255.0*0.99)+0.01
+    targets=numpy.zeros(output_nodes)+0.01
+    targets[int(all_values[0])]=0.99
+    n.train(inputs,targets)
+    pass
+
+
+test_data_file=open("mnist_dataset/mnist_test_10.csv",'r')
+test_data_list=test_data_file.readlines()
+test_data_file.close()
+
+all_values2= test_data_list[0].split(',')
+print(all_values2[0])
+
+image_array=numpy.asfarray(all_values2[1:]).reshape((28,28))
+matplotlib.pyplot.imshow(image_array, cmap='Greys', interpolation='None')
+matplotlib.pyplot.show()
+
+print(n.query((numpy.asfarray(all_values2[1:])/255.0*0.99)+0.01))
+
+
+
+
 
